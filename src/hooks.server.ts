0a69cc229f0f129d338f5f6a6f5be4de27d11afa
Dev0n Lamond <43188519+dlamon1@users.noreply.github.com>
2023-01-20 18:10:45 -0500

    Update hooks.server.ts

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -5,3 +5,5 @@ import { GITHUB_ID, GITHUB_SECRET } from "$env/static/private"
 export const handle = SvelteKitAuth({
   providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })],
 })
+
+// Pretty funny that you're taking the entire hook function for yourself. Like the amount of people that will not be utilizing the handle function in hooks.server.ts will be close to 0.
